9184c39fd1a34b9acca0b4cf2dca8f3f15872ea9
Chris <me@lifeoncruzcontrol.com>
2024-10-13 13:02:56 -0700

    Specified host

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ const fastify = Fastify({
 });
 
 fastify.get('/', async (req, res) => {
-	res.send({'Hello world'});
+	res.send({ msg: "Hello world!" })
 });
 
 const start = async () => {
 	try {
-		await fastify.listen({ port: 3000 });
+		await fastify.listen({ port: 3000, host: "0.0.0.0" });
 	} catch (err) {
 		fastify.log.error(err);
 		process.exit(1);
